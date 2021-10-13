4b6a196a3b6233bee394f81edf3bb849f981a249
Derek Araujo <derekaraujo19@gmail.com>
2021-10-13 11:52:00 -0400

    commit tree d82bbf6be633c9adfff0601471800a3a3ab07b85

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+// grab the counter
+let counter = document.getElementById('counter');
+counter.addEventListener('click', () =>{
+  console.log('click')
+})
+
+
+
+
+
+
+
+})
+
+
+
